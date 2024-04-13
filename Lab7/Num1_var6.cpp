3a415b704c66ca6bc6a6f022b1813435a2e5d2c8
Nikita <notname.nam@gmail.com>
2024-04-13 19:05:06 +0300

    fix num1, num3

--- Lab7/Num1_var6.cpp
+++ Lab7/Num1_var6.cpp
@@ -2,11 +2,13 @@
 //#include <fstream>
 //#include <string>
 //#include <vector>
+//#include <iostream>
 //
 //int WinWidth = 640,
 //WinHeight = 480;
 //int WinFar = 10;
 //int xx, yy, xxx, yyy;
+//
 //
 //void DrawPoint(int x, int y)
 //{
@@ -22,29 +24,32 @@
 //	glLineWidth(6);
 //	glColor3f(0, 0, 0);
 //	glBegin(GL_LINES);
-//	
 //	glVertex2f(x1, y1);
 //	glVertex2f(x2, y2);
 //	glEnd();
-//}
-//
-//void display(int x1, int y1)
-//{
-//	glClear(GL_COLOR_BUFFER_BIT);
-//	Draws
-//	DrawPoint(x1, y1);
-//	
-//	
 //}
 //
 //struct Point {
 //	int x;
 //	int y;
 //};
-//void g() {
+//
+//std::vector <Point> Points;
+//
+//void Display() {
 //	glClear(GL_COLOR_BUFFER_BIT);
+//	DrawLine(-100, 0, 100, 0);
+//	DrawLine(0, -100, 0, 100);
+//	for (int i = 0; i < Points.size(); ++i){
+//		DrawPoint(Points[i].x, Points[i].y);
+//	}
+//	DrawLine(xx, yy, xxx, yyy);
+//	glEnd();
+//	glFlush();
+//}
+//
+//void Start() {
 //	std::ifstream in("input.txt");
-//	std::vector <Point> Points;
 //
 //	while (!in.eof()) {
 //		Point temp;
@@ -54,7 +59,6 @@
 //		temp.x = atoi(x.c_str());
 //		temp.y = atoi(y.c_str());
 //		Points.push_back(temp);
-//		DrawPoint(temp.x, temp.y);
 //	}
 //	in.close();
 //
@@ -63,7 +67,7 @@
 //		int is_down = 0;
 //		int diff = 10000;
 //		for (int i = 0; i < Points.size() - 1; ++i) {
-//			for (int j = i+1; j < Points.size(); ++j) { 
+//			for (int j = i + 1; j < Points.size(); ++j) {
 //				int ij_x = Points[j].x - Points[i].x;
 //				int ij_y = Points[j].y - Points[i].y;
 //				for (int l = 0; l < Points.size(); ++l) {
@@ -72,29 +76,31 @@
 //					else if (normal_equation < 0) ++is_down;
 //				}
 //				if (diff > abs(is_up - is_down)) {
-//					diff = abs(is_up - is_down); // точку саму с собой сравнивает получает точку а не линию
+//					diff = abs(is_up - is_down);
 //					/*x1 =  Points[i].x;
 //					y1 = Points[i].y;
 //					x2 = Points[j].x;
 //					y2 = Points[j].y;*/
 //					xx = 100;
-//					Points[j].x - Points[i].x != 0 ? yy = ij_y * (100 - Points[i].x) / (Points[j].x - Points[i].x) + Points[i].y: yy = Points[i].y;
+//					Points[j].x - Points[i].x != 0 ? yy = ij_y * (xx - Points[i].x) / (Points[j].x - Points[i].x) + Points[i].y : yy = 100, xx = Points[i].x;
 //					xxx = -100;
-//					Points[j].x - Points[i].x != 0 ? yyy = ij_y * (-100 - Points[i].x) / (Points[j].x + Points[i].x) + Points[i].y: yyy = Points[i].y;
-//					
+//					Points[j].x - Points[i].x != 0 ? yyy = ij_y * (xxx - Points[i].x) / (Points[j].x + Points[i].x) + Points[i].y : yyy = -100, xxx = Points[j].x;
+//
 //				}
 //				is_up = 0;
 //				is_down = 0;
 //			}
 //		}
-//		DrawLine(xx, yy, xxx, yyy);
 //	}
-//	glEnd();
-//	glFlush();
+//	else {
+//		exit(0);
+//		std::cout << "incorrect value";
+//	}
 //}
 //
 //int main(int argc, char** argv)
 //{
+//	Start();
 //	glutInit(&argc, argv);
 //	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 //	glutInitWindowSize(WinWidth, WinHeight);
@@ -105,7 +111,7 @@
 //    glLoadIdentity();
 //    gluOrtho2D(-100.0, 100.0, -100.0, 100.0);
 //	
-//	glutDisplayFunc(g);
+//	glutDisplayFunc(Display);
 //
 //	glutMainLoop();
 //	return 0;
--- Lab7/Num3_var7.cpp
+++ Lab7/Num3_var7.cpp
@@ -1,55 +1,72 @@
-//#include "glut.h"
-//#include <stdlib.h>
-//#include <vector>
-//#include <iostream>
-//
-//std::vector <int> x;
-//std::vector <int> y;
-//int n = 0;
-//
-//void DrowPoint(std::vector <int>& x, std::vector <int>& y) {
-//	for (int i = 0; i < x.size(); ++i) {
-//		glBegin(GL_POINTS);
-//		glVertex2f(x[i], y[i]);
-//		glEnd();
-//		
-//	}
-//}
-//
-//void renderScene(void) {
-//	if (x.size() > 500) {
-//		std::cout << (double)x.size() / (double)n;
-//		exit(1);
-//	}
-//	x.push_back(rand() % 200 - 100);
-//	y.push_back(rand() % 200 - 100);
-//	if (-pow(x.back(), 3) + pow(y.back(), 3) < 2 &&
-//		-x.back() + y.back() < 1 &&
-//		-2 < x.back() < 2 &&
-//		-2 < y.back() < 2) n++;
-//	glClear(GL_COLOR_BUFFER_BIT);
-//	glColor3f(1.0f, 1.0f, 1.0f);
-//
-//	glEnd();
-//	DrowPoint(x, y);
-//	glutSwapBuffers();
-//}
-//
-//int main(int argc, char** argv) {
-//	
-//	// инициализаци€
-//	glutInit(&argc, argv);
-//	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-//	glutInitWindowPosition(100, 100);
-//	glutInitWindowSize(400, 400);
-//	glutCreateWindow("Num3");
-//	glMatrixMode(GL_PROJECTION);
-//	glLoadIdentity();
-//	gluOrtho2D(-100, 100, -100, 100); // нормальные координаты а не через float
-//	// регистраци€ обратных вызовов
-//	glutDisplayFunc(renderScene);
-//	glutIdleFunc(renderScene);
-//	// ќсновной цикл GLUT
-//	glutMainLoop();
-//	return 0;
-//}
+#include "glut.h"
+#include <stdlib.h>
+#include <vector>
+#include <iostream>
+
+std::vector <int> x;
+std::vector <int> y;
+double n = 0;
+
+void DrowPoint() {
+	for (int i = 0; i < x.size(); ++i) {
+		
+		if ((-pow(x[i], 3) + pow(x[i], 3) < 2) &&
+			(-x[i] + y[i] < 1) &&
+			(-2 < x[i] < 2) &&
+			(-2 < y[i] < 2)) {
+			glBegin(GL_POINTS);
+			glColor3f(1, 1, 0);
+			glVertex2f(x[i], y[i]);
+			glEnd();
+		}
+		else {
+			
+			glBegin(GL_POINTS);
+			glColor3f(1, 0, 0);
+			glVertex2f(x[i], y[i]);
+			glEnd();
+		}
+		
+	}
+}
+
+
+void Display(void) {
+	glClear(GL_COLOR_BUFFER_BIT);
+	
+	if (x.size() > 500) {
+		std::cout << n / x.size() * 400;
+		exit(1);
+	}
+	DrowPoint();
+	
+	x.push_back(rand() % 200 - 100);
+	y.push_back(rand() % 200 - 100);
+	if ((-pow(x.back(), 3) + pow(y.back(), 3) < 2) &&
+		(-x.back() + y.back() < 1) &&
+		(-2 < x.back() < 2) &&
+		(-2 < y.back() < 2)) n++;
+	glutSwapBuffers();
+}
+void timer(int = 0) {
+	Display();
+	glutTimerFunc(20, timer, 0);
+}
+
+
+int main(int argc, char** argv) {
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(400, 400);
+	glutCreateWindow("Num3");
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-100, 100, -100, 100);
+	glutDisplayFunc(Display);
+	glutTimerFunc(10, timer, 0);
+	glutMainLoop();
+
+	return 0;
+}
